65fc03f49c543c5bbea77ca40e348532a9390a28
Orion Dobos <oriondobos@gmail.com>
2023-01-07 18:46:18 -0700

    menu updates

--- Assets/Scripts/MenuScripts/Menus.cs
+++ Assets/Scripts/MenuScripts/Menus.cs
@@ -8,23 +8,44 @@ public class Menus : MonoBehaviour
     [Header("Buttons")]
     public Button inventoryButton;
     public Button closeInventoryButton;
+    public Button marketButton;
 
     [Header("Menus")]
     public GameObject inventoryMenu;
+    public GameObject marketMenu;
 
     private void Start()
     {
         inventoryButton.onClick.AddListener(delegate
             {
-                inventoryMenu.gameObject.SetActive(true);
+                //buttons
                 inventoryButton.gameObject.SetActive(false);
+                marketButton.gameObject.SetActive(false);
+                //menus
+                inventoryMenu.gameObject.SetActive(true);
+                
             }
         );
 
         closeInventoryButton.onClick.AddListener(delegate
             {
-                inventoryMenu.gameObject.SetActive(false);
+                //buttons
                 inventoryButton.gameObject.SetActive(true);
+                marketButton.gameObject.SetActive(true);
+                //menus
+                inventoryMenu.gameObject.SetActive(false);
+                marketMenu.gameObject.SetActive(false);
+            }
+        );
+
+        marketButton.onClick.AddListener(delegate
+            {
+                //buttons
+                inventoryButton.gameObject.SetActive(false);
+                marketButton.gameObject.SetActive(false);
+                //menus
+                inventoryMenu.gameObject.SetActive(true);
+                marketMenu.gameObject.SetActive(true);
             }
         );
     }
